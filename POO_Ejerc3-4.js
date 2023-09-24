6787f0163d8bf230778f26dfb8b48b331a1cf4a5
Antonio Gennaro <antgennaro@hotmail.com>
2023-09-24 00:01:00 -0300

    POO Ejerc. 3-4 Uso de rutinas varias y limpiez lineas comentadas

--- POO_Ejerc3-4.js
+++ POO_Ejerc3-4.js
@@ -7,56 +7,23 @@ const { reporteClientes,
         reporteVentasA,
         estadoProductosClientes }= require("./rutinas.js");
 //
-//// Funciones locales
-/**
- * Consulta estado de Productos y Clientes
- */
-//function estadoProductosClientes() {
-//    clientes[0].montoGastado();
-//    clientes[1].montoGastado();
-// Estado de Stock 
-//    productos[0].cuantoStockTienes();
-//    productos[1].cuantoStockTienes();
-// Reporte de Ventas
-//    productos[0].reporteDeVentas();
-//    productos[1].reporteDeVentas();
-//}
-/// Presentación de Productos y Clientes...
+//  SUPERMERCADO DEVS (Manejo de Stock)
+//
+/// Presentación de Productos y Clientes registrados...
 //
 console.log("DÍA '0' PRESENTACIÓN DE PRODUCTOS Y CLIENTES");
 reporteProductos();
 reporteClientes();
-//console.log("Producto 1:");
-//productos[0].cualEstuNombre();
-//productos[0].cuantoStockTienes();
 //
-//console.log("Producto 2:");
-//productos[1].cualEstuNombre();
-//productos[1].cuantoStockTienes();
-//
-//console.log("Cliente 1:");
-//clientes[0].cualEstuNombre();
-//clientes[0].montoGastado();
-//
-//console.log("Cliente 2:");
-//clientes[1].cualEstuNombre();
-//clientes[1].montoGastado();
-//
-// Interacciones  PRIMER DÍA
+// Ventas PRIMER DÍA
 // Ciente1 compra 1Kg de Yerba y 1 Kg de Azucar
-// Liente2 compra 1kg de Azucar
-// 
+// Cliente2 compra 1kg de Azucar
 console.log("DÍA '1' Ventas del Día");
-// Cliente 1 Operación 1
-//cliente1.comprarProducto(producto1, 1);
 productos[0].venderACliente(clientes[0], 1);
-// Cliente 1 operación 2
-//cliente1.comprarProducto(producto2, 1);
 productos[1].venderACliente(clientes[0], 1);
-// Cliente 2 operación 1
 productos[1].venderACliente(clientes[1], 1);
 //
-console.log("Cierre del día 1");
+console.log("Estado al cierre del día 1");
 estadoProductosClientes();
 /////////////////////////////////
 // Dia 2
@@ -64,7 +31,7 @@ console.log("DÍA '2' Ventas del Día");
 productos[0].venderACliente(clientes[0], 2);
 productos[0].venderACliente(clientes[1], 3);
 /////
-console.log("Cierre del día 2");
+console.log("Estado al cierre del día 2");
 estadoProductosClientes();
 /////////////
 // Dia 3
@@ -72,7 +39,7 @@ console.log("DÍA '3' Ventas del Día");
 productos[1].venderACliente(clientes[0], 2);
 productos[0].venderACliente(clientes[1], 2);
 /////
-console.log("Cierre del día 3");
+console.log("Estado al cierre del día 3");
 estadoProductosClientes();
 //
 /* Fin */

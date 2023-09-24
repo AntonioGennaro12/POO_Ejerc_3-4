cee718b072465b2cbcf0cd038d8bc4f18615a571
Antonio Gennaro <antgennaro@hotmail.com>
2023-09-23 23:32:31 -0300

    POO Ejerc. 3-4 agrego módulo de rutinas varias

--- POO_Ejerc3-4.js
+++ POO_Ejerc3-4.js
@@ -2,39 +2,45 @@
 // Importo las definiciones de clases
 const clientes = require("./defClientes.js");
 const productos = require("./defProductos.js");
+const { reporteClientes,
+        reporteProductos,
+        reporteVentasA,
+        estadoProductosClientes }= require("./rutinas.js");
 //
 //// Funciones locales
 /**
  * Consulta estado de Productos y Clientes
  */
-function estadoProductosClientes() {
-    clientes[0].montoGastado();
-    clientes[1].montoGastado();
-    // Estado de Stock 
-    productos[0].cuantoStockTienes();
-    productos[1].cuantoStockTienes();
-    // Reporte de Ventas
-    productos[0].reporteDeVentas();
-    productos[1].reporteDeVentas();
-}
+//function estadoProductosClientes() {
+//    clientes[0].montoGastado();
+//    clientes[1].montoGastado();
+// Estado de Stock 
+//    productos[0].cuantoStockTienes();
+//    productos[1].cuantoStockTienes();
+// Reporte de Ventas
+//    productos[0].reporteDeVentas();
+//    productos[1].reporteDeVentas();
+//}
 /// Presentación de Productos y Clientes...
 //
 console.log("DÍA '0' PRESENTACIÓN DE PRODUCTOS Y CLIENTES");
-console.log("Producto 1:");
-productos[0].cualEstuNombre();
-productos[0].cuantoStockTienes();
+reporteProductos();
+reporteClientes();
+//console.log("Producto 1:");
+//productos[0].cualEstuNombre();
+//productos[0].cuantoStockTienes();
 //
-console.log("Producto 2:");
-productos[1].cualEstuNombre();
-productos[1].cuantoStockTienes();
+//console.log("Producto 2:");
+//productos[1].cualEstuNombre();
+//productos[1].cuantoStockTienes();
 //
-console.log("Cliente 1:");
-clientes[0].cualEstuNombre();
-clientes[0].montoGastado();
+//console.log("Cliente 1:");
+//clientes[0].cualEstuNombre();
+//clientes[0].montoGastado();
 //
-console.log("Cliente 2:");
-clientes[1].cualEstuNombre();
-clientes[1].montoGastado();
+//console.log("Cliente 2:");
+//clientes[1].cualEstuNombre();
+//clientes[1].montoGastado();
 //
 // Interacciones  PRIMER DÍA
 // Ciente1 compra 1Kg de Yerba y 1 Kg de Azucar
@@ -68,8 +74,7 @@ productos[0].venderACliente(clientes[1], 2);
 /////
 console.log("Cierre del día 3");
 estadoProductosClientes();
-
-/////////////
+//
 /* Fin */
 
 
--- defClientes.js
+++ defClientes.js
@@ -1,7 +1,5 @@
 const Cliente = require("./Cliente.js");
 
-
-
 const clientes = [ cliente1 = new Cliente("Juan", 0),
                    cliente2 = new Cliente("Pedro", 0) ];
 
